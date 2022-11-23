1ad2153c8ec454f59b3b0a86398c1c2b429a0f69
skyriv213 <gn1007@dankook.ac.kr>
2022-11-23 11:00:26 +0900

    Delete 기능구현

--- memo/src/main/java/MemoList.java
+++ memo/src/main/java/MemoList.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import java.util.Scanner;
 
 public class MemoList {
 
@@ -17,7 +18,30 @@ public class MemoList {
     }
 
     public void delete() {
-
+        while (true) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("삭제하실 글의 번호를 입력해주십쇼. 해당 기능을 종료하시려면 0000를 눌러주시길바랍니다.");
+            String idx0 = sc.next();
+            if (idx0 == "0000"){
+                break;
+            }
+            int idx = Integer.parseInt(idx0);
+            if (memoList.contains(idx)) {
+                System.out.println("해당 글의 비밀번호를 입력해주십쇼");
+                int pw = sc.nextInt();
+                Memo memoAction = getMemo(idx);
+                if (memoAction.getPassword() == pw) {
+                    memoList.remove(idx);
+                    System.out.println("삭제가 완료되었습니다.");
+                }
+                else {
+                    System.out.println("비밀번호가 틀렸습니다. 처음부터 다시 진행해주십쇼");
+                }
+            }
+            else{
+                System.out.println("해당 글의 번호가 존재하지 않습니다.");
+            }
+        }
     }
 
     public void showAll() {
