accb2513845779b9703f1449718447f4da9f90f6
sssssjj <sshsjhome@gmail.com>
2022-11-23 11:00:35 +0900

    add content field

--- memo/src/main/java/Memo.java
+++ memo/src/main/java/Memo.java
@@ -3,34 +3,43 @@ public class Memo {
     private int number;
     private String name;
     private int password;
+    private String content;
     private String date;
 
     public int getNumber() {
         return number;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getPassword() {
         return password;
     }
 
-    public void setPassword(int password) {
-        this.password = password;
+    public String getContent() {
+        return content;
     }
 
     public String getDate() {
         return date;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
     }
 
     public void setDate(String date) {
