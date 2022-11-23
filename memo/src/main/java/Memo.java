ed149db1d1e497a77af9e404c021e331077d6ba8
sssssjj <sshsjhome@gmail.com>
2022-11-23 10:14:18 +0900

    add getter

--- memo/src/main/java/Memo.java
+++ memo/src/main/java/Memo.java
@@ -5,5 +5,35 @@ public class Memo {
     private int password;
     private String date;
 
+    public int getNumber() {
+        return number;
+    }
 
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPassword() {
+        return password;
+    }
+
+    public void setPassword(int password) {
+        this.password = password;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
