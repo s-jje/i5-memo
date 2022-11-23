f8addc4edc6830a6740a86545feff4d49787f33a
sssssjj <sshsjhome@gmail.com>
2022-11-23 10:01:35 +0900

    first commit

---
+++ memo/src/main/java/Memo.java
@@ -0,0 +1,9 @@
+public class Memo {
+
+    private int number;
+    private String name;
+    private int password;
+    private String date;
+
+
+}
---
+++ memo/src/main/java/MemoList.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+public class MemoList {
+
+    private List<Memo> memoList;
+
+    public MemoList(List<Memo> memoList) {
+        this.memoList = memoList;
+    }
+
+    public void create() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public void delete() {
+
+    }
+
+    public void showAll() {
+
+    }
+
+    public boolean isExist(int memoNumber) {
+        return false;
+    }
+}
